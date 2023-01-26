649312df6b448540867b2ad4fa77f61e18c80fd2
Frédéric Helfer <fre.helfer@gmail.com>
2023-01-26 16:14:59 -0300

    implemented NSCache

--- RickAndMorty/ViewModels/RMCharacterCollectionViewCellViewModel.swift
+++ RickAndMorty/ViewModels/RMCharacterCollectionViewCellViewModel.swift
@@ -13,16 +13,6 @@ final class RMCharacterCollectionViewCellViewModel: Hashable {
     private let characterStatus: RMCharacterStatus
     private let characterImageUrl: URL?
     
-    static func == (lhs: RMCharacterCollectionViewCellViewModel, rhs: RMCharacterCollectionViewCellViewModel) -> Bool {
-        return lhs.hashValue == rhs.hashValue
-    }
-    
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(characterName)
-        hasher.combine(characterStatus)
-        hasher.combine(characterImageUrl)
-    }
-    
     // MARK: - Init
     
     init(characterName: String, characterStatus: RMCharacterStatus, characterImageUrl: URL?) {
@@ -36,23 +26,23 @@ final class RMCharacterCollectionViewCellViewModel: Hashable {
     }
     
     public func fetchImage(completion: @escaping (Result<Data, Error>) -> Void) {
-        
-        // TODO: Abstract to image manager
-        
         guard let url = characterImageUrl else {
             completion(.failure(URLError(.badURL)))
             return
         }
         
-        let request = URLRequest(url: url)
-        
-        let task = URLSession.shared.dataTask(with: request) { data, _, error in
-            guard let data = data, error == nil else {
-                completion(.failure(error ?? URLError(.badServerResponse)))
-                return
-            }
-            completion(.success(data))
-        }
-        task.resume()
+        RMImageLoader.shared.downloadImage(url, completion: completion)
+    }
+    
+    // MARK: - Hashable
+    
+    static func == (lhs: RMCharacterCollectionViewCellViewModel, rhs: RMCharacterCollectionViewCellViewModel) -> Bool {
+        return lhs.hashValue == rhs.hashValue
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(characterName)
+        hasher.combine(characterStatus)
+        hasher.combine(characterImageUrl)
     }
 }
